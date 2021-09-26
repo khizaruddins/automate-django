f84f50af8eb9f93a7e70904d848bddefe6d9134c
Greg Drake <greg@madmallards.com>
2021-09-26 14:53:50 -0400

    Changed staticfiles to static
    Updated the HTML template output to include static rather than staticfiles.

--- automate_django/automate_django.py
+++ automate_django/automate_django.py
@@ -250,7 +250,7 @@ footer{
     with open(os.getcwd() + '/' + currentProject + '/templates/' + currentApp + '/base.html', 'w') as f:
         base_string = """
 <!DOCTYPE html>
-{{% load staticfiles %}}
+{{% load static %}}
 <html lang="en" dir="ltr">
 <head>
 <meta charset="utf-8">
@@ -282,7 +282,7 @@ footer{
 
     with open(os.getcwd() + '/' + currentProject + '/templates/' + currentApp + '/partials/head.html', 'w') as f:
         f.write('''
-{% load staticfiles %}
+{% load static %}
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="{% static 'css/style.css' %}">
 ''')
@@ -355,7 +355,7 @@ this is copyright
 
     with open(os.getcwd() + '/' + currentProject + '/templates/' + currentApp + '/partials/script.html', 'w') as f:
         script_string = """
-        {% load staticfiles %}
+        {% load static %}
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
